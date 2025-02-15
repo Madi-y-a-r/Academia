a8263f266882f6f346946bc03c6f1141af9b7fe9
Madiyar <madiyar.galymbek@gmail.com>
2025-02-15 15:38:14 +0500

    Fix: Build Lang

--- client/app/[locale]/(nondashboard)/checkout/completion/index.tsx
+++ client/app/[locale]/(nondashboard)/checkout/completion/index.tsx
@@ -2,33 +2,35 @@
 
 import { Button } from "@/components/ui/button";
 import { Check } from "lucide-react";
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 import React from "react";
 
 const CompletionPage = () => {
+  const t = useTranslations("Payment")
   return (
     <div className="completion">
       <div className="completion__content">
         <div className="completion__icon">
           <Check className="w-16 h-16" />
         </div>
-        <h1 className="completion__title">COMPLETED</h1>
+        <h1 className="completion__title">{t("CONGRATULATIONS")}</h1>
         <p className="completion__message">
-          🎉 You have made a course purchase successfully! 🎉
+          🎉 {t("competionMessage")} 🎉
         </p>
       </div>
       <div className="completion__support">
         <p>
-          Need help? Contact our{" "}
+          {t("Help")}{" "}
           <Button variant="link" asChild className="p-0 m-0 text-primary-700">
-            <a href="mailto:madiyar.galymbek@gamil.com">customer support</a>
+            <a href="mailto:madiyar.galymbek@gamil.com">{t("customer support")}</a>
           </Button>
           .
         </p>
       </div>
       <div className="completion__action">
-        <Link href="student/courses" scroll={false}>
-          Go to Courses
+        <Link href="student/courses" className="font-bold" scroll={false}>
+          {t("Go to Courses")}
         </Link>
       </div>
     </div>
